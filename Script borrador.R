4d21a458c6fd88f3f871b931451268c562c9feac
unknown <bfbellagamba@gmail.com>
2018-06-07 09:59:10 -0300

    7/6

--- Script borrador.R
+++ Script borrador.R
@@ -1,5 +1,7 @@
+library(tidyverse)
 library(readr)
-
+library(shiny)
+library(mosaic)
 ##Armando la base
 Este_2013 <- read_csv("Siniestros/2013/Este_2013.txt")
 Oeste_2013 <- read_csv("Siniestros/2013/Oeste_2013.txt")
@@ -31,7 +33,44 @@ base2014<-rbind(Este_2014,Oeste_2014,Norte_2014,Montevideo_2014)
 base2015<-rbind(Este_2015,Oeste_2015,Norte_2015,Montevideo_2015)
 base2016<-rbind(Este_2016,Oeste_2016,Norte_2016,Montevideo_2016)
 base2017<-rbind(Este_2017,Oeste_2017,Norte_2017,Montevideo_2017)
+#Crear variable año
+base2013<-base2013%>%mutate(año=2013)
+base2014<-base2014%>%mutate(año=2014)
+base2015<-base2015%>%mutate(año=2015)
+base2016<-base2016%>%mutate(año=2016)
+base2017<-base2017%>%mutate(año=2017)
 
 datos<-rbind(base2013,base2014,base2015,base2016,base2017)
 
+
 summary(datos)
+
+
+##Graficos de variables cualitativas count
+
+ggplot(datos,aes(x = datos$`Tipo de Siniestro`)) + geom_bar()
+ggplot(datos,aes(x = Gravedad)) + geom_bar()
+ggplot(datos,aes(x = datos$`Dia Semana`)) + geom_bar()
+ggplot(datos,aes(x = Departamento)) + geom_bar()
+ggplot(datos,aes(x = año)) + geom_bar()
+ggplot(datos,aes(x = Hora)) + geom_bar()
+
+
+
+
+
+
+ggplot(datos,aes(x = datos$`Tipo de Siniestro`,fill=Gravedad)) + geom_bar(position = "fill")
+ggplot(datos,aes(x = Departamento,fill=Gravedad)) + geom_bar(position = "fill")
+ggplot(datos,aes(x = año,fill=Gravedad)) + geom_bar(position = "fill")#Parece que hay una tendencia en los no lesionados
+ggplot(datos,aes(fill = datos$`Tipo de Siniestro`,x=año)) + geom_bar(position = "fill")
+
+
+
+
+
+
+ggplot(datos) + geom_mosaic(aes(weight=, x=product(datos$`Tipo de Siniestro`,datos$`Dia Semana`) ,fill=Gravedad,colour=Gravedad))
+
+prueba<-datos%>%select(datos$`Tipo de Siniestro`,Gravedad)
+
