f5abca6714b410ee99ee9b693c047565f64eb227
unknown <bfbellagamba@gmail.com>
2018-06-18 09:40:33 -0300

    18/6

--- Proyectosiniestros/app.R
+++ Proyectosiniestros/app.R
@@ -1,4 +1,11 @@
 library(shiny)
+library(tidyverse)
+library(rgdal)
+library(raster)
+library(maptools)
+library(broom)
+library(ggmosaic)
+library(lubridate)
 
 ui <- fluidPage(
   titlePanel("Siniestros en el periodo 2013-2017"),
@@ -151,7 +158,8 @@ server <- function(input, output) {
       }
         
     )
-    
+  output$mapa<-renderPlot(
+    if(input$selecanual == "Todos" & input$selects == "Todos" & input$selecgrav == "Todas"){
     count<-datos%>%group_by(Departamento)%>%summarise(n=n())
     count<-count$n
     uruguay <- getData("GADM", country = "UY", level = 0)
@@ -179,65 +187,61 @@ server <- function(input, output) {
     countunique <-uystates_df %>%
       group_by(NAME_1) %>%
       summarise(mlong = mean(long), mlat = mean(lat))
-    output$mapa<-renderPlot(
-      # if(input$selecanual == "Todos" & input$selects == "Todos" & input$selecgrav == "Todos"){count<-datos%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual == "Todos" & input$selects != "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())
-      # count<-count$n
-      # uruguay <- getData("GADM", country = "UY", level = 0)
-      # uruguay_states <- getData("GADM", country = "UY", level = 1)
-      # uystates_UTM <-spTransform(uruguay_states, CRS("+init=EPSG:5383")) 
-      # NAME_1 <- uystates_UTM@data$NAME_1
-      # count_df <- data.frame(NAME_1, count)
-      # uystates_UTM@data$id <- rownames(uystates_UTM@data)
-      # uystates_UTM@data <- plyr::join(uystates_UTM@data, count_df, by="NAME_1")
-      # uystates_df <- tidy(uystates_UTM)
-      # uystates_df <- plyr::join(uystates_df,uystates_UTM@data, by="id")
-      # uystates_df <-uystates_df %>% filter(!(NAME_1=="Rivera"& lat<6400000)) #un error en el mapa que hay que sacar
-      # theme_opts <- list(theme(panel.grid.minor = element_blank(),
-      #                          panel.grid.major = element_blank(),
-      #                          panel.background = element_blank(),
-      #                          plot.background = element_blank(),
-      #                          axis.line = element_blank(),
-      #                          axis.text.x = element_blank(),
-      #                          axis.text.y = element_blank(),
-      #                          axis.ticks = element_blank(),
-      #                          axis.title.x = element_blank(),
-      #                          axis.title.y = element_blank(),
-      #                          plot.title = element_blank()))
-      # 
-      # countunique <-uystates_df %>%
-      #   group_by(NAME_1) %>%
-      #   summarise(mlong = mean(long), mlat = mean(lat))
-      # ggplot() +
-      #   geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
-      #                                          count), color = "black", size = 0.25) +
-      #   geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
-      #   theme(aspect.ratio = 1) + labs(fill = "Cantidad")+
-      #   scale_fill_gradient2( midpoint = mean(count),low = "red", mid = "white",
-      #                         high = "blue") +
-      #   theme_opts}
-      # else if(input$selecanual != "Todos" & input$selects == "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Año==input$selecanual)%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual == "Todos" & input$selects == "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Gravedad==input$selecgrav)%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual == "Todos" & input$selects != "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Gravedad==input$selecgrav & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual != "Todos" & input$selects == "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Gravedad==input$selecgrav & Año==input$selecanual)%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Año==input$selecanual & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
-      # else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Año==input$selecanual & Gravedad==input$selecgrav & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
-
-      
-
-      ggplot() +
-        geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
-                                               count), color = "black", size = 0.25) +
-        geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
-        theme(aspect.ratio = 1) + labs(fill = "Cantidad")+
-        scale_fill_gradient2( midpoint = mean(count),low = "red", mid = "white",
-                              high = "blue") +
-        theme_opts
+    ggplot() +
+      geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
+                                             count), color = "black", size = 0.25) +
+      geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
+      theme(aspect.ratio = 1) + labs(fill = "Cantidad")+
+      scale_fill_gradient2( midpoint = mean(count),low = "red", mid = "white",
+                            high = "blue") +
+      theme_opts
+  
+    }
+    else if(input$selecanual == "Todos" & input$selects != "Todos" & input$selecgrav == "Todas"){
+    count<-datos%>%filter(Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())    
+    count<-count$n
+    uruguay <- getData("GADM", country = "UY", level = 0)
+    uruguay_states <- getData("GADM", country = "UY", level = 1)
+    uystates_UTM <-spTransform(uruguay_states, CRS("+init=EPSG:5383")) 
+    NAME_1 <- uystates_UTM@data$NAME_1
+    count_df <- data.frame(NAME_1, count)
+    uystates_UTM@data$id <- rownames(uystates_UTM@data)
+    uystates_UTM@data <- plyr::join(uystates_UTM@data, count_df, by="NAME_1")
+    uystates_df <- tidy(uystates_UTM)
+    uystates_df <- plyr::join(uystates_df,uystates_UTM@data, by="id")
+    uystates_df <-uystates_df %>% filter(!(NAME_1=="Rivera"& lat<6400000)) #un error en el mapa que hay que sacar
+    theme_opts <- list(theme(panel.grid.minor = element_blank(),
+                             panel.grid.major = element_blank(),
+                             panel.background = element_blank(),
+                             plot.background = element_blank(),
+                             axis.line = element_blank(),
+                             axis.text.x = element_blank(),
+                             axis.text.y = element_blank(),
+                             axis.ticks = element_blank(),
+                             axis.title.x = element_blank(),
+                             axis.title.y = element_blank(),
+                             plot.title = element_blank()))
     
-    )
+    countunique <-uystates_df %>%
+      group_by(NAME_1) %>%
+      summarise(mlong = mean(long), mlat = mean(lat))
+    ggplot() +
+      geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
+                                             count), color = "black", size = 0.25) +
+      geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
+      theme(aspect.ratio = 1) + labs(fill = "Cantidad")+
+      scale_fill_gradient2( midpoint = mean(count),low = "red", mid = "white",
+                            high = "blue") +
+      theme_opts}
+    # else if(input$selecanual == "Todos" & input$selects != "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Gravedad==input$selecgrav & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
+    # else if(input$selecanual != "Todos" & input$selects == "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Gravedad==input$selecgrav & Año==input$selecanual)%>%group_by(Departamento)%>%summarise(n=n())}
+    # else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Año==input$selecanual & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
+    # else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Año==input$selecanual & Gravedad==input$selecgrav & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
+  )
+   
+    
     
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
--- Script borrador LU.R
+++ Script borrador LU.R
@@ -189,7 +189,7 @@ datos %>%
 
 
 count<-datos%>%group_by(Departamento)%>%summarise(n=n())
-count<-datos%>%filter(Tipo_de_siniestro == "DESPISTE")%>%group_by(Departamento)%>%summarise(n=n())
+count<-datos%>%filter(Año=="2017" & Gravedad=="LEVE" & Tipo_de_siniestro=="DESPISTE")%>%group_by(Departamento)%>%summarise(n=n())
 count<-count$n
 uruguay <- getData("GADM", country = "UY", level = 0)
 uruguay_states <- getData("GADM", country = "UY", level = 1)
@@ -216,7 +216,7 @@ theme_opts <- list(theme(panel.grid.minor = element_blank(),
 countunique <-uystates_df %>%
   group_by(NAME_1) %>%
   summarise(mlong = mean(long), mlat = mean(lat))
-ggplot() +
+a<-ggplot() +
   geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
                                          count), color = "black", size = 0.25) +
   geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
@@ -236,3 +236,39 @@ else if(input$selecanual != "Todos" & input$selects == "Todos" & input$selecgrav
 else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Año==input$selecanual & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
 else if(input$selecanual != "Todos" & input$selects != "Todos" & input$selecgrav != "Todos"){count<-datos%>%filter(Año==input$selecanual & Gravedad==input$selecgrav & Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())}
 
+# if(input$selecanual == "Todos" & input$selects == "Todos" & input$selecgrav == "Todos"){count<-datos%>%group_by(Departamento)%>%summarise(n=n())}
+# else if(input$selecanual == "Todos" & input$selects != "Todos" & input$selecgrav == "Todos"){count<-datos%>%filter(Tipo_de_siniestro==input$selects)%>%group_by(Departamento)%>%summarise(n=n())
+# count<-count$n
+# uruguay <- getData("GADM", country = "UY", level = 0)
+# uruguay_states <- getData("GADM", country = "UY", level = 1)
+# uystates_UTM <-spTransform(uruguay_states, CRS("+init=EPSG:5383")) 
+# NAME_1 <- uystates_UTM@data$NAME_1
+# count_df <- data.frame(NAME_1, count)
+# uystates_UTM@data$id <- rownames(uystates_UTM@data)
+# uystates_UTM@data <- plyr::join(uystates_UTM@data, count_df, by="NAME_1")
+# uystates_df <- tidy(uystates_UTM)
+# uystates_df <- plyr::join(uystates_df,uystates_UTM@data, by="id")
+# uystates_df <-uystates_df %>% filter(!(NAME_1=="Rivera"& lat<6400000)) #un error en el mapa que hay que sacar
+# theme_opts <- list(theme(panel.grid.minor = element_blank(),
+#                          panel.grid.major = element_blank(),
+#                          panel.background = element_blank(),
+#                          plot.background = element_blank(),
+#                          axis.line = element_blank(),
+#                          axis.text.x = element_blank(),
+#                          axis.text.y = element_blank(),
+#                          axis.ticks = element_blank(),
+#                          axis.title.x = element_blank(),
+#                          axis.title.y = element_blank(),
+#                          plot.title = element_blank()))
+# 
+# countunique <-uystates_df %>%
+#   group_by(NAME_1) %>%
+#   summarise(mlong = mean(long), mlat = mean(lat))
+# ggplot() +
+#   geom_polygon(data = uystates_df, aes(x = long, y = lat, group = group, fill =
+#                                          count), color = "black", size = 0.25) +
+#   geom_text(data =countunique ,aes(label = round(count,2), x = mlong, y = mlat))+
+#   theme(aspect.ratio = 1) + labs(fill = "Cantidad")+
+#   scale_fill_gradient2( midpoint = mean(count),low = "red", mid = "white",
+#                         high = "blue") +
+#   theme_opts}
